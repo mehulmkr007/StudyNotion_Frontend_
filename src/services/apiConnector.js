f099b09cf385ed2be142ad3bdfeb8e98a11ae750
mehulRaghvani <mehulmkr007@gmail.com>
2026-02-24 12:27:33 +0530

    bug fixing & testing

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -5,7 +5,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({});
 
-export const apiConnector = (method, url, bodyData, headers, params) => {
+export const 
+apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
     method: `${method}`,
     url: `${url}`,
